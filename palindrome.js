5551b5f258edcb7003db4d8ce5018262470a4712
Edilbek <edil.talantbekov@gmail.com>
2020-08-24 14:04:07 +0600

    add fix palindrome

--- palindrome.js
+++ palindrome.js
@@ -1,38 +1,18 @@
 var isPalindrome = function (x) {
-  // let number = x.toString();
-  // let start = 0;
-  // let end = number.length - 1;
-  // while (start < end) {
-  //   console.log(number.charAt(start++));
-  //   console.log(number.charAt(end--));
-  //   if (number.charAt(start++) !== number.charAt(end--)) {
-  //     return false;
-  //   }
-  // }
-  // return true;
+  if (x < 0) return false;
 
-  let arr1 = [];
-  let arr2 = [];
-
-  let numbers = Array.from(String(x), Number);
-  let length = x.toString().length;
-
-  for (let i = 0; i < length; i++) {
-    arr1.push(numbers[i]);
-    for (let j = length; j--; ) {
-      arr2.push(numbers[j]);
-    }
-  }
-
-  for (let i = length; i--; ) {
-    arr2.push(numbers[i]);
-  }
-
-  for (let i = 0; i < length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-
-  return true;
+  return x === reversedInteger(x);
 };
 
-console.log(isPalindrome(-121));
+var reversedInteger = function (x) {
+  let reversed = 0;
+
+  while (x > 0) {
+    reversed = reversed * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return reversed;
+};
+
+console.log(isPalindrome(121));
